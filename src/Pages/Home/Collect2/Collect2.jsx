e9914431136e57594ee3d92e552c9b2420ad1172
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-01-30 10:39:12 +0500

    first commit7

--- src/Pages/Home/Collect2/Collect2.jsx
+++ src/Pages/Home/Collect2/Collect2.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Collect2.css";
 import fly from "../../../Assets/my_fly.svg";
 import shape from "../../../Assets/circle/Elements-16.png";
@@ -11,35 +11,109 @@ import img6 from "../../../Assets/circle/Elements-07.png";
 import img7 from "../../../Assets/circle/Elements-08.png";
 import letter from "../../../Assets/a.svg";
 function Collect2() {
+  const data = [
+    {
+      identifier: "wallet",
+      name: "Wallet",
+      img: img6,
+      desc: `The mystery box will be available for $10, each box will have either
+            of Five different NFT rarities. Holders will be able to sell their
+            NFTs on Arcane Market Place.`,
+    },
+    {
+      identifier: "meta",
+      name: "Metaverse",
+      img: img3,
+      desc: `The mystery box will be available for $10, each box will have either
+            of Five different NFT rarities. Holders will be able to sell their
+            NFTs on Arcane Market Place.`,
+    },
+    {
+      identifier: "store",
+      name: "Store",
+      img: img,
+      desc: `The mystery box will be available for $10, each box will have either
+            of Five different NFT rarities. Holders will be able to sell their
+            NFTs on Arcane Market Place.`,
+    },
+    {
+      identifier: "box",
+      name: "Mystery Box",
+      img: img5,
+      desc: `The mystery box will be available for $10, each box will have either
+            of Five different NFT rarities. Holders will be able to sell their
+            NFTs on Arcane Market Place.`,
+    },
+    {
+      identifier: "mobile",
+      name: "Mobile",
+      img: img4,
+      desc: `The mystery box will be available for $10, each box will have either
+            of Five different NFT rarities. Holders will be able to sell their
+            NFTs on Arcane Market Place.`,
+    },
+    {
+      identifier: "cards",
+      name: "Cards",
+      img: img2,
+      desc: `The mystery box will be available for $10, each box will have either
+            of Five different NFT rarities. Holders will be able to sell their
+            NFTs on Arcane Market Place.`,
+    },
+    {
+      identifier: "sword",
+      name: "Sword",
+      img: img7,
+      desc: `The mystery box will be available for $10, each box will have either
+            of Five different NFT rarities. Holders will be able to sell their
+            NFTs on Arcane Market Place.`,
+    },
+  ];
+  const [imgstate, setImg] = useState(img6);
+  const [name, setName] = useState("Wallet");
+  const [desc, setDesc] =
+    useState(`The mystery box will be available for $10, each box will have either
+            of Five different NFT rarities. Holders will be able to sell their
+            NFTs on Arcane Market Place.`);
+  const clickHandler = (e) => {
+    const dataSet = e.target.dataset.text;
+    data.forEach((item) => {
+      if (item.identifier === dataSet) {
+        setImg(item.img);
+        setName(item.name);
+        setDesc(item.desc);
+      }
+    });
+  };
   return (
     <div className="collect2">
       <div data-aos="fade-right" className="collect2_left">
         <img src={shape} alt="" />
-        <div data-text="wallet" className="collect-box">
+        <div onClick={clickHandler} data-text="wallet" className="collect-box">
           <img src={img6} alt="" />
           <p>WALLET</p>
         </div>
-        <div data-text="meta" className="collect-box">
+        <div onClick={clickHandler} data-text="meta" className="collect-box">
           <img src={img3} alt="" />
           <p>METAVERSE</p>
         </div>
-        <div data-text="store" className="collect-box">
+        <div onClick={clickHandler} data-text="store" className="collect-box">
           <img src={img} alt="" />
           <p>STORE</p>
         </div>
-        <div data-text="box" className="collect-box">
+        <div onClick={clickHandler} data-text="box" className="collect-box">
           <img src={img5} alt="" />
           <p>MYSTERY BOX</p>
         </div>
-        <div data-text="mobile" className="collect-box">
+        <div onClick={clickHandler} data-text="mobile" className="collect-box">
           <img src={img4} alt="" />
           <p>MOBILE</p>
         </div>
-        <div data-text="cards" className="collect-box">
+        <div onClick={clickHandler} data-text="cards" className="collect-box">
           <img src={img2} alt="" />
           <p>CARDS</p>
         </div>
-        <div data-text="sword" className="collect-box">
+        <div onClick={clickHandler} data-text="sword" className="collect-box">
           <img src={img7} alt="" />
           <p>SWORD</p>
         </div>
@@ -55,14 +129,10 @@ function Collect2() {
         </div>
         <div data-aos="fade-up" data-aos-delay="1000" className="right-box">
           <div className="box-top">
-            <img src={fly} alt="" />
-            <p>Mystery Box</p>
+            <img src={imgstate} alt="" />
+            <p>{name}</p>
           </div>
-          <p>
-            The mystery box will be available for $10, each box will have either
-            of Five different NFT rarities. Holders will be able to sell their
-            NFTs on Arcane Market Place.
-          </p>
+          <p>{desc}</p>
           <button>
             <img src={letter} alt="" /> COMING SOON
           </button>
@@ -74,14 +144,10 @@ function Collect2() {
         className="right-box mobile-box"
       >
         <div className="box-top">
-          <img src={fly} alt="" />
-          <p>Mystery Box</p>
+          <img src={imgstate} alt="" />
+          <p>{name}</p>
         </div>
-        <p>
-          The mystery box will be available for $10, each box will have either
-          of Five different NFT rarities. Holders will be able to sell their
-          NFTs on Arcane Market Place.
-        </p>
+        <p>{desc}</p>
         <button>
           <img src={letter} alt="" /> COMING SOON
         </button>
--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -1,10 +1,36 @@
 import React from "react";
 import "./Hero.css";
 import bg from "../../../Assets/hero_img.svg";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/splide/dist/css/themes/splide-default.min.css";
 function Hero() {
   return (
     <div className="hero">
-      <img src={bg} alt="" />
+      <Splide
+        options={{
+          interval: 3000,
+          autoplay: true,
+          type: "loop",
+          rewind: true,
+          loop: true,
+          perPage: 1,
+          height: "auto",
+          width: "100%",
+          arrows: false,
+          pagination: true,
+          drag: true,
+        }}
+      >
+        <SplideSlide>
+          <img src={bg} alt="" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src={bg} alt="" />
+        </SplideSlide>
+        <SplideSlide>
+          <img src={bg} alt="" />
+        </SplideSlide>
+      </Splide>
     </div>
   );
 }

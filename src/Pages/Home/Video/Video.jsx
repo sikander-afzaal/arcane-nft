95d3076e4f2052a4bfae951ba2efe98dbcd3485f
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-01-31 18:07:31 +0500

    first commit9

--- src/Pages/Home/Video/Video.jsx
+++ src/Pages/Home/Video/Video.jsx
@@ -1,26 +1,26 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Video.css";
 import vidImg from "../../../Assets/Thumbnail.png";
 import play from "../../../Assets/play.png";
 function Video() {
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
   return (
     <div data-aos="zoom-in" data-aos-delay="1000" className="video">
-      <img className="vid" src={vidImg} alt="" />
-      <img onClick={() => setOpen(true)} src={play} alt="" className="play" />
+      <iframe
+        width="1280"
+        height="720"
+        src="https://www.youtube.com/embed/wcvER4WfMlg"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+      {/* <img onClick={() => setOpen(true)} src={play} alt="" className="play" />
       <div className={`watching ${open ? "open-vid" : ""}`}>
         <div className={`overlay-3 ${open ? "open-vid" : ""}`}></div>
-        <i onClick={() => setOpen(false)} className="far fa-times"></i>
-        <iframe
-          width="1280"
-          height="720"
-          src="https://www.youtube.com/embed/wcvER4WfMlg"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
+        <i onClick={() => setOpen(false)} className="far fa-times"></i> */}
+
+      {/* </div> */}
     </div>
   );
 }

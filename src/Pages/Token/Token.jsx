2ec24801e965225ee722905079c71771f2f32d5b
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-02-01 18:52:23 +0500

    first commit8

--- src/Pages/Token/Token.jsx
+++ src/Pages/Token/Token.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Token.css";
 import Token_hero from "./Token_hero/Token_hero.jsx";
 import Audit from "./Audit/Audit.jsx";
+import Tokenomics from "./Tokenomics/Tokenomics.jsx";
 //assets
 
 function Token() {
@@ -9,6 +10,7 @@ function Token() {
     <div className="token">
       <Token_hero />
       <Audit />
+      <Tokenomics />
     </div>
   );
 }

873e012e86a92f7fb4ce4e84f81fbe42c78290d3
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-02-06 22:41:48 +0500

    first commit

--- src/Pages/Home/Hero/Hero.jsx
+++ src/Pages/Home/Hero/Hero.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./Hero.css";
 import bg from "../../../Assets/banners/b1.png";
 import bgMobile from "../../../Assets/banners/b1-mobile.png";
-import bg2 from "../../../Assets/banners/b2.mp4";
-import bg2Mobile from "../../../Assets/banners/b2-mobile.mp4";
-import bg3 from "../../../Assets/banners/b3.mp4";
-import bg3Mobile from "../../../Assets/banners/b3-mobile.mp4";
+import bg2 from "../../../Assets/banners/b2.png";
+import bg2Mobile from "../../../Assets/banners/b2-mobile.png";
+import bg3 from "../../../Assets/banners/b3.png";
+import bg3Mobile from "../../../Assets/banners/b3-mobile.png";
 import bg4 from "../../../Assets/banners/b4.png";
 import bg4Mobile from "../../../Assets/banners/b4-mobile.png";
+import bg5 from "../../../Assets/banners/b5.png";
+import bg5Mobile from "../../../Assets/banners/b5-mobile.png";
 
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/splide/dist/css/themes/splide-default.min.css";
@@ -16,8 +18,8 @@ function Hero() {
     <div className="hero">
       <Splide
         options={{
-          // interval: 3000,
-          // autoplay: true,
+          interval: 3000,
+          autoplay: true,
           type: "loop",
           rewind: true,
           loop: true,
@@ -34,48 +36,24 @@ function Hero() {
           <img className="mobile-bg" src={bgMobile} alt="" />
         </SplideSlide>
         <SplideSlide>
-          <video
-            className="desktop-bg"
-            controls={false}
-            muted={true}
-            autoPlay={true}
-            src={bg2}
-            loop={true}
-          ></video>
-          <video
-            className="mobile-bg"
-            controls={false}
-            muted={true}
-            autoPlay={true}
-            src={bg2Mobile}
-            loop={true}
-          ></video>
+          <img className="desktop-bg" src={bg2} alt="" />
+          <img className="mobile-bg" src={bg2Mobile} alt="" />
         </SplideSlide>
         <SplideSlide>
-          <video
-            className="desktop-bg"
-            controls={false}
-            muted={true}
-            autoPlay={true}
-            src={bg3}
-            loop={true}
-          ></video>
-          <video
-            className="mobile-bg"
-            controls={false}
-            muted={true}
-            autoPlay={true}
-            src={bg3Mobile}
-            loop={true}
-          ></video>
+          <img className="desktop-bg" src={bg3} alt="" />
+          <img className="mobile-bg" src={bg3Mobile} alt="" />
+        </SplideSlide>
+        <SplideSlide>
+          <img className="desktop-bg" src={bg4} alt="" />
+          <img className="mobile-bg" src={bg4Mobile} alt="" />
         </SplideSlide>
         <SplideSlide>
           <a
             href="https://pancakeswap.finance/swap?outputCurrency=0x477565b356b3973d16e8cd837c6970613f13e24a"
             target={"blank"}
           >
-            <img className="desktop-bg" src={bg4} alt="" />
-            <img className="mobile-bg" src={bg4Mobile} alt="" />
+            <img className="desktop-bg" src={bg5} alt="" />
+            <img className="mobile-bg" src={bg5Mobile} alt="" />
           </a>
         </SplideSlide>
       </Splide>
